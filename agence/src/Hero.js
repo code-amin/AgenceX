82031f951757d2085cedf7fabc0d427863212e8d
Mhammed Doukkali <mhammed.doukkali@outlook.com>
2023-12-15 16:36:41 -0500

    hero and services respo

--- agence/src/Hero.js
+++ agence/src/Hero.js
@@ -2,16 +2,16 @@ import { FcIdea } from "react-icons/fc";
 
 const Hero = () => {
   return (
-    <div className="bg-gray-900 text-white py-20 flex flex-wrap items-center">
-      <div className="max-w-md mx-auto md:max-w-2xl md:mx-0 md:w-1/2 md:order-2">
-        <FcIdea className="text-yellow-500" size={500} style={{ margin: "0 auto" }} />
+    <div className="bg-gray-900 text-white py-10 md:py-20 flex flex-col md:flex-row items-center">
+      <div className="max-w-md mx-auto md:max-w-none md:w-1/2 md:order-2 md:ml-auto md:mr-0">
+        <FcIdea className="text-yellow-500 w-20 md:w-32 lg:w-40 h-20 md:h-32 lg:h-40" size={500} style={{ margin: "0 auto" }}  />
       </div>
-      <div className="max-w-md mx-auto md:max-w-2xl md:mx-0 md:w-1/2 md:order-1">
+      <div className="max-w-md mx-auto md:max-w-none md:w-1/2 md:order-1">
         <div className="p-4">
-          <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-8">
+          <h1 className="text-3xl md:text-5xl lg:text-6xl font-bold mb-4 md:mb-8">
             Donnez vie à vos idées avec des solutions technologiques innovantes
           </h1>
-          <p className="text-lg md:text-xl lg:text-2xl leading-relaxed">
+          <p className="text-base md:text-xl lg:text-2xl leading-relaxed">
             Propulsez votre entreprise vers demain grâce à nos solutions évolutives et adaptées.
             Notre agence offre des solutions personnalisées adaptées à vos besoins spécifiques,
             offrant ainsi des réponses concrètes à vos défis numériques.
--- agence/src/Services.js
+++ agence/src/Services.js
@@ -2,39 +2,37 @@ import { FaHeadphones, FaSearch, FaLightbulb, FaCogs, FaChartLine } from "react-
 
 const Services = () => {
   return (
-    <div className="py-20 bg-gray-900 " >
-      <div className="container px-4 mx-auto">
-        <div className="grid grid-cols-5 gap-20 max-w-5xl mx-auto">
-          <div className="flex flex-col items-center space-y-4">
-            <div className="rounded-full bg-white p-4">
-              <FaHeadphones size={100} />
-            </div>
-            <p className="text-center text-white">Écoute attentive pour cerner les besoins spécifiques.</p>
+    <div className="py-10 bg-gray-900">
+      <div className="container mx-auto flex flex-col items-center space-y-8">
+        <div className="flex flex-col items-center space-y-4">
+          <div className="icon-container">
+            <FaHeadphones size={80} className="icon text-white border border-white rounded-full p-3" />
           </div>
-          <div className="flex flex-col items-center space-y-4">
-            <div className="rounded-full bg-white p-4" style={{ transform: 'translateY(5px)' }}>
-              <FaSearch size={100} />
-            </div>
-            <p className="text-center text-white" >Examen minutieux de la situation, concurrents, opportunités.</p>
+          <p className="text-center text-white">Écoute attentive pour cerner les besoins spécifiques.</p>
+        </div>
+        <div className="flex flex-col items-center space-y-4">
+          <div className="icon-container">
+            <FaSearch size={80} className="icon text-white border border-white rounded-full p-3" />
           </div>
-          <div className="flex flex-col items-center space-y-4">
-            <div className="rounded-full bg-white p-4" style={{ transform: 'translateY(20px)' }}>
-              <FaLightbulb size={100} />
-            </div>
-            <p className="text-center text-white">Développement stratégique adapté à vos besoins et marché.</p>
+          <p className="text-center text-white">Examen minutieux de la situation, concurrents, opportunités.</p>
+        </div>
+        <div className="flex flex-col items-center space-y-4">
+          <div className="icon-container">
+            <FaLightbulb size={80} className="icon text-white border border-white rounded-full p-3" />
           </div>
-          <div className="flex flex-col items-center space-y-4">
-            <div className="rounded-full bg-white p-4" style={{ transform: 'translateY(25px)' }}>
-              <FaCogs size={100} />
-            </div>
-            <p className="text-center text-white">Mise en œuvre précise des solutions convenues.</p>
+          <p className="text-center text-white">Développement stratégique adapté à vos besoins et marché.</p>
+        </div>
+        <div className="flex flex-col items-center space-y-4">
+          <div className="icon-container">
+            <FaCogs size={80} className="icon text-white border border-white rounded-full p-3" />
           </div>
-          <div className="flex flex-col items-center space-y-4">
-            <div className="rounded-full bg-white p-4" style={{ transform: 'translateY(40px)' }}>
-              <FaChartLine size={100} />
-            </div>
-            <p className="text-center text-white">Analyse, ajustement pour une croissance constante et efficace.</p>
+          <p className="text-center text-white">Mise en œuvre précise des solutions convenues.</p>
+        </div>
+        <div className="flex flex-col items-center space-y-4">
+          <div className="icon-container">
+            <FaChartLine size={80} className="icon text-white border border-white rounded-full p-3" />
           </div>
+          <p className="text-center text-white">Analyse, ajustement pour une croissance constante et efficace.</p>
         </div>
       </div>
     </div>
