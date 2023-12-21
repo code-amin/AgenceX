efa472c122157019f9ae5a42a01f290099c93022
AlphaHelloWorld <73975219+AlphaHelloWorld@users.noreply.github.com>
2023-12-20 22:06:43 -0500

    changed menu side because people are right handed

--- agence/src/Header.js
+++ agence/src/Header.js
@@ -9,63 +9,52 @@ const Header = () => {
   };
 
   return (
-    <header className="HEADER w-full p-4 text-2xl fixed flex text-white bg-gray-900 shadow-lg">
-      <RxHamburgerMenu
-        className={
-          "HAMBURGER flex-0 mr-10 transform transition-transform duration-500 ease-in-out md:hidden " +
-          (isMenuOpen ? "rotate-90 " : "rotate-0 ")
-        }
-        size="32px"
-        onClick={handleHamburger}
-      />
-
-      {isMenuOpen && (
-        <div
-          className={
-            "MOBILE-MENU absolute top-16 transition-transform left-0 h-[100vh] duration-500 ease-in-out w-5/6 bg-gray-900 shadow-2xl " +
-            (isMenuOpen ? "visible" : "hidden")
-          }
-        >
-          <ul className="">
-            <li>
-              <a
-                href="/#realizations"
-                className="block hover:text-3xl transition-transform duration-1000 ease-in-out transform hover:translate-x-1 pl-4 py-3 hover:cursor-pointer"
-                onClick={handleHamburger}
-              >
-                Realizations
-              </a>
-            </li>
-            <li>
-              <a
-                href="/#services"
-                className="block hover:text-3xl transition-transform duration-800 ease-in-out transform hover:translate-x-1 pl-4 py-3 hover:cursor-pointer"
-                onClick={handleHamburger}
-              >
-                Services
-              </a>
-            </li>
-            <li>
-              <a
-                href="/#pricing"
-                className="block hover:text-3xl transition-transform duration-800 ease-in-out transform hover:translate-x-1 pl-4 py-3 hover:cursor-pointer"
-                onClick={handleHamburger}
-              >
-                Pricing
-              </a>
-            </li>
-            <li>
-              <a
-                href="/Blog"
-                className="block hover:text-3xl transition-transform duration-800 ease-in-out transform hover:translate-x-1 pl-4 py-3 hover:cursor-pointer"
-                onClick={handleHamburger}
-              >
-                Blog
-              </a>
-            </li>
-          </ul>
-        </div>
-      )}
+    <header className="HEADER w-full p-4 text-2xl fixed flex text-white bg-gray-900 shadow-lg transition-transform">
+      <div className="flex-1"></div>
+      <div
+        className={`MOBILE-MENU absolute top-16 right-0 translate-transition h-[100vh] w-5/6 bg-gray-900 shadow-2xl ${
+          isMenuOpen ? "translate-x-0" : "translate-x-full"
+        } ease-in-out duration-300 `}
+      >
+        <ul className="">
+          <li>
+            <a
+              href="/#realizations"
+              className="block hover:text-3xl transition-transform duration-1000 ease-in-out transform hover:translate-x-1 pl-4 py-3 hover:cursor-pointer"
+              onClick={handleHamburger}
+            >
+              Realizations
+            </a>
+          </li>
+          <li>
+            <a
+              href="/#services"
+              className="block hover:text-3xl transition-transform duration-800 ease-in-out transform hover:translate-x-1 pl-4 py-3 hover:cursor-pointer"
+              onClick={handleHamburger}
+            >
+              Services
+            </a>
+          </li>
+          <li>
+            <a
+              href="/#pricing"
+              className="block hover:text-3xl transition-transform duration-800 ease-in-out transform hover:translate-x-1 pl-4 py-3 hover:cursor-pointer"
+              onClick={handleHamburger}
+            >
+              Pricing
+            </a>
+          </li>
+          <li>
+            <a
+              href="/Blog"
+              className="block hover:text-3xl transition-transform duration-800 ease-in-out transform hover:translate-x-1 pl-4 py-3 hover:cursor-pointer"
+              onClick={handleHamburger}
+            >
+              Blog
+            </a>
+          </li>
+        </ul>
+      </div>
 
       <a href="/" className={"flex "}>
         <div className="LOGO border-[1px] p-1 ">
@@ -75,7 +64,15 @@ const Header = () => {
         </div>
         <div className="border-[1px] p-1 ">Licorne Labs</div>
       </a>
-      <div className="flex-1"></div>
+
+      <RxHamburgerMenu
+        className={
+          "HAMBURGER flex-0 ml-10 transform transition-transform duration-500 ease-in-out md:hidden " +
+          (isMenuOpen ? "rotate-90 " : "rotate-0 ")
+        }
+        size="32px"
+        onClick={handleHamburger}
+      />
     </header>
   );
 };
