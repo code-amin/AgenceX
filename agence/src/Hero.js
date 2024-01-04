bd9437e0f3d40b36551202f5053d5954f2e34906
Mhammed Doukkali <mhammed.doukkali@outlook.com>
2024-01-04 11:29:09 -0500

    add react spring icon animation to hero

--- agence/src/Hero.js
+++ agence/src/Hero.js
@@ -1,10 +1,49 @@
+import { useState, useEffect } from "react";
 import { FcIdea } from "react-icons/fc";
 
+
+
+import {useSpring, animated} from 'react-spring';
+
 const Hero = () => {
+
+  const [dropped, setDropped] = useState(false);
+  const [showColors, setShowColors] = useState(false);
+
+  useEffect(() => {
+    const dropTimer = setTimeout(() => {
+      setDropped(true);
+    }, 1000);
+  
+    return () => clearTimeout(dropTimer);
+  }, []);
+
+  const dropAnimation = useSpring({
+    from: { y: -500 },
+    to: { y: dropped ? 0 : -500 },
+    config: { tension: 100, friction: 40 },
+    onRest: () => {
+      // Once the drop animation finishes, trigger color animation
+      setShowColors(true);
+    },
+  });
+
+
   return (
     <div className="bg-gray-900 text-white pt-20 md:py-20 flex flex-col md:flex-row items-center">
       <div className="max-w-md mx-auto md:max-w-none md:w-1/2 md:order-2 md:ml-auto md:mr-0">
-        <FcIdea className="text-yellow-500 w-20 md:w-32 lg:w-40 h-20 md:h-32 lg:h-40" size={500} style={{ margin: "0 auto" }}  />
+      <animated.div
+        style={{
+          ...dropAnimation,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <FcIdea className={` w-20 md:w-32 lg:w-40 h-20 md:h-32 lg:h-40 ${
+            showColors ? 'grayscale-0' :'grayscale-[50%]'
+          }`} size={600} style={{ margin: "0 auto" }}  />
+        </animated.div>
       </div>
       <div className="max-w-md mx-auto md:max-w-none md:w-1/2 md:order-1">
         <div className="p-4">
