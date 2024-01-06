9cf79c2d85c6dfd3989822e68ab06692a302d7d8
AlphaHelloWorld <73975219+AlphaHelloWorld@users.noreply.github.com>
2024-01-06 11:08:07 -0500

    remove process.js

--- agence/src/App.js
+++ agence/src/App.js
@@ -5,7 +5,7 @@ import Realizations from "./Realizations";
 import Services from "./Services";
 import Arguments from "./Arguments";
 import Reviews from "./Reviews";
-import Process from "./Process";
+// import Process from "./Process";
 import Pricing from "./Pricing";
 import Contact from "./Contact";
 import Footer from "./Footer";
@@ -27,7 +27,7 @@ const App = () => {
       <Services />
       <Arguments />
       <Reviews />
-      <Process />
+      {/* <Process /> */}
       <Pricing onSelectPlan={handlePlanSelection} plan={plan} />
       <Contact plan={plan} />
       <Socials />
--- agence/src/Process.js
+++ agence/src/Process.js
@@ -1,26 +1,26 @@
-const Process = () => {
-  return (
-    <div id="process">
-      <h2 className="text-2xl font-bold text-center mb-4">
-        Our 3 Step Process
-      </h2>
-      <div className="flex flex-col md:flex-row gap-4 justify-center">
-        <div className="bg-gray-800 p-6 rounded-lg">
-          <p className="text-white-500 mb-4">1. Understanding your needs </p>
-        </div>
-        <div className="bg-gray-800 p-6 rounded-lg">
-          <p className="text-white-500 mb-4">
-            2. Delivery of a first solution{" "}
-          </p>
-        </div>
-        <div className="bg-gray-800 p-6 rounded-lg">
-          <p className="text-white-500 mb-4">
-            3. Adjustments and final approval from you{" "}
-          </p>
-        </div>
-      </div>
-    </div>
-  );
-};
+// const Process = () => {
+//   return (
+//     <div id="process">
+//       <h2 className="text-2xl font-bold text-center mb-4">
+//         Our 3 Step Process
+//       </h2>
+//       <div className="flex flex-col md:flex-row gap-4 justify-center">
+//         <div className="bg-gray-800 p-6 rounded-lg">
+//           <p className="text-white-500 mb-4">1. Understanding your needs </p>
+//         </div>
+//         <div className="bg-gray-800 p-6 rounded-lg">
+//           <p className="text-white-500 mb-4">
+//             2. Delivery of a first solution{" "}
+//           </p>
+//         </div>
+//         <div className="bg-gray-800 p-6 rounded-lg">
+//           <p className="text-white-500 mb-4">
+//             3. Adjustments and final approval from you{" "}
+//           </p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Process;
+// export default Process;
