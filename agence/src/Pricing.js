6cda6dfffb228cd11e2b799998a6db75e27d55ae
Mhammed Doukkali <mhammed.doukkali@outlook.com>
2024-01-10 16:51:03 -0500

    pricing added

--- agence/src/Pricing.js
+++ agence/src/Pricing.js
@@ -4,33 +4,33 @@ const Pricing = ({ onSelectPlan, plan }) => {
   };
 
   const basic = {
-    // name: "Basic",
-    // price: "29$",
-    // pages: "5",
-    // support: "Basic Support",
-    // features: "Limited Features",
+    name: "Basic",
+    price: "29$",
+    pages: "5 pages",
+    support: "Basic Support",
+    features: "Limited Features",
     fr: "Plan de base - 29$",
   };
   const standard = {
-    // name: "Standard",
-    // price: "59$",
-    // pages: "10",
-    // support: "Standard Support",
-    // features: "More Features",
+    name: "Standard",
+    price: "59$",
+    pages: "10 pages",
+    support: "Standard Support",
+    features: "More Features",
     fr: "Plan Standard - 59$",
   };
   const premium = {
-    // name: "Premium",
-    // price: "99$",
-    // pages: "Unlimited",
-    // support: "Priority Support",
-    // features: "Advanced Features",
+    name: "Premium",
+    price: "99$",
+    pages: "Unlimited pages",
+    support: "Priority Support",
+    features: "Advanced Features",
     fr: "Plan Premium - 99$",
   };
   return (
     <div id="pricing" className="bg-gray-900 py-8 pt-24">
       <h2 className="text-2xl font-bold text-white text-center mb-8">
-        Our Pricing
+        Tarification
       </h2>
       <div className="text-black flex flex-col md:flex-row justify-center mx-4">
         {/* Package 1 */}
@@ -44,13 +44,13 @@ const Pricing = ({ onSelectPlan, plan }) => {
             handlePlan(basic.fr);
           }}
         >
-          <div className="py-6 px-4 hover:cursor-pointer">
-            <h3 className="text-lg font-semibold text-center mb-4">Basic</h3>
-            <p className="text-2xl font-bold text-center mb-4">$29</p>
+          <div className="py-6 px-4 hover:cursor-pointer text-center" >
+            <h3 className="text-lg font-semibold text-center mb-4">{basic.name}</h3>
+            <p className="text-2xl font-bold text-center mb-4">{basic.price}</p>
             <ul className="text-sm">
-              <li className="mb-2">5 Pages Website</li>
-              <li className="mb-2">Basic Support</li>
-              <li className="mb-2">Limited Features</li>
+              <li className="mb-2">{basic.pages}</li>
+              <li className="mb-2">{basic.support}</li>
+              <li className="mb-2">{basic.features}</li>
             </ul>
             <button className="block mx-auto bg-yellow-500 text-white py-2 px-4 mt-6 rounded-md hover:bg-yellow-400">
               Get Started
@@ -60,7 +60,7 @@ const Pricing = ({ onSelectPlan, plan }) => {
 
         {/* Package 2 */}
         <div
-          className={`bg-white shadow-md rounded-lg w-full md:w-1/3 md:mx-4 mb-4 hover:cursor-pointer ${
+          className={`bg-white shadow-md rounded-lg w-full md:w-1/3 md:mx-4 mb-4 ${
             plan === standard.fr
               ? "outline outline-offset-4 outline-yellow-400 "
               : "outline-none"
@@ -69,13 +69,13 @@ const Pricing = ({ onSelectPlan, plan }) => {
             handlePlan(standard.fr);
           }}
         >
-          <div className="py-6 px-4">
-            <h3 className="text-lg font-semibold text-center mb-4">Standard</h3>
-            <p className="text-2xl font-bold text-center mb-4">$59</p>
+          <div className="py-6 px-4 hover:cursor-pointer text-center" >
+            <h3 className="text-lg font-semibold text-center mb-4">{standard.name}</h3>
+            <p className="text-2xl font-bold text-center mb-4">{standard.price}</p>
             <ul className="text-sm">
-              <li className="mb-2">10 Pages Website</li>
-              <li className="mb-2">Standard Support</li>
-              <li className="mb-2">More Features</li>
+              <li className="mb-2">{standard.pages}</li>
+              <li className="mb-2">{standard.support}</li>
+              <li className="mb-2">{standard.features}</li>
             </ul>
             <button className="block mx-auto bg-yellow-500 text-white py-2 px-4 mt-6 rounded-md hover:bg-yellow-400">
               Get Started
@@ -85,7 +85,7 @@ const Pricing = ({ onSelectPlan, plan }) => {
 
         {/* Package 3 */}
         <div
-          className={`bg-white shadow-md rounded-lg w-full md:w-1/3 md:mx-4 mb-4 hover:cursor-pointer ${
+          className={`bg-white shadow-md rounded-lg w-full md:w-1/3 md:mx-4 mb-4 ${
             plan === premium.fr
               ? "outline outline-offset-4 outline-yellow-400 "
               : "outline-none"
@@ -94,13 +94,13 @@ const Pricing = ({ onSelectPlan, plan }) => {
             handlePlan(premium.fr);
           }}
         >
-          <div className="py-6 px-4">
-            <h3 className="text-lg font-semibold text-center mb-4">Premium</h3>
-            <p className="text-2xl font-bold text-center mb-4">$99</p>
+          <div className="py-6 px-4 hover:cursor-pointer text-center" >
+            <h3 className="text-lg font-semibold text-center mb-4">{premium.name}</h3>
+            <p className="text-2xl font-bold text-center mb-4">{premium.price}</p>
             <ul className="text-sm">
-              <li className="mb-2">Unlimited Pages</li>
-              <li className="mb-2">Priority Support</li>
-              <li className="mb-2">Advanced Features</li>
+              <li className="mb-2">{premium.pages}</li>
+              <li className="mb-2">{premium.support}</li>
+              <li className="mb-2">{premium.features}</li>
             </ul>
             <button className="block mx-auto bg-yellow-500 text-white py-2 px-4 mt-6 rounded-md hover:bg-yellow-400">
               Get Started
