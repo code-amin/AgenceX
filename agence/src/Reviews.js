75e6d254f576ebb4e9cde6efb80cf70cf098afd6
Mhammed Doukkali <mhammed.doukkali@outlook.com>
2024-01-10 15:47:09 -0500

    add stars to reviews

--- agence/src/Reviews.js
+++ agence/src/Reviews.js
@@ -1,25 +1,49 @@
+import { FaStar } from 'react-icons/fa';
+
 const Reviews = () => {
   return (
-    <div id="testimonials" className="bg-gray-900 text-white px-8">
+    <div id="testimonials" className="bg-gray-900 text-white px-8 py-12 text-center">
       <h2 className="text-2xl font-bold mb-4">Client Reviews</h2>
       <div className="flex flex-col md:flex-row gap-4 justify-center">
-        <div className="bg-gray-800 p-6 rounded-lg">
-          <p className="text-yellow-500 mb-4">
+        <div className="bg-gray-800 p-6 rounded-lg flex items-center flex-col">
+          <div className="flex items-center">
+            <FaStar className="text-yellow-500" />
+            <FaStar className="text-yellow-500" />
+            <FaStar className="text-yellow-500" />
+            <FaStar className="text-yellow-500" />
+            <FaStar className="text-yellow-500" />
+          </div>
+          <p className="text-yellow-500 ml-2">
             "Fantastic service! Highly recommended!"
           </p>
-          <p className="text-sm">- John Doe</p>
+          <p className="text-sm ml-2">- John Doe</p>
         </div>
-        <div className="bg-gray-800 p-6 rounded-lg">
-          <p className="text-yellow-500 mb-4">
+        <div className="bg-gray-800 p-6 rounded-lg flex items-center flex-col">
+          <div className="flex items-center">
+            <FaStar className="text-yellow-500" />
+            <FaStar className="text-yellow-500" />
+            <FaStar className="text-yellow-500" />
+            <FaStar className="text-yellow-500" />
+            <FaStar className="text-yellow-500" />
+
+          </div>
+          <p className="text-yellow-500 ml-2">
             "Great experience working with this agency."
           </p>
-          <p className="text-sm">- Jane Smith</p>
+          <p className="text-sm ml-2">- Jane Smith</p>
         </div>
-        <div className="bg-gray-800 p-6 rounded-lg">
-          <p className="text-yellow-500 mb-4">
+        <div className="bg-gray-800 p-6 rounded-lg flex items-center flex-col">
+          <div className="flex items-center">
+            <FaStar className="text-yellow-500" />
+            <FaStar className="text-yellow-500" />
+            <FaStar className="text-yellow-500" />
+            <FaStar className="text-yellow-500" />
+            <FaStar className="text-yellow-500" />
+          </div>
+          <p className="text-yellow-500 ml-2">
             "Amazing experience working with the team"
           </p>
-          <p className="text-sm">- Sarah Johnson</p>
+          <p className="text-sm ml-2">- Sarah Johnson</p>
         </div>
       </div>
     </div>
--- agence/src/Services.js
+++ agence/src/Services.js
@@ -2,40 +2,40 @@
 
 const Services = () => {
   return (
-    <section className=" py-12">
+    <section id="services" className=" py-24">
       <div className="container mx-auto">
         <h2 className="text-4xl font-bold text-center mb-8">Our Services</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {/* Custom app development */}
           <div className="p-6 shadow-lg rounded-lg relative">
             <h3 className="text-xl font-semibold mb-4 relative">Custom App Development
-              <span className="absolute left-0 bottom-[-0.55556rem] bg-[#e7e729] rounded-[0.11111rem] content-[''] h-[0.22222rem] transition-all duration-250 ease w-[1.83333rem]"></span>
+              <span className="absolute left-0 bottom-[-0.55556rem] bg-yellow-500 rounded-[0.11111rem] content-[''] h-[0.22222rem] transition-all duration-250 ease w-[1.83333rem]"></span>
             </h3> 
           </div>
           {/* Product design (UX/UI) */}
           <div className="p-6  shadow-lg rounded-lg relative">
             <h3 className="text-xl font-semibold mb-4 relative">Product Design (UX/UI)
-            <span className="absolute left-0 bottom-[-0.55556rem] bg-[#e7e729] rounded-[0.11111rem] content-[''] h-[0.22222rem] transition-all duration-250 ease w-[1.83333rem]"></span>
+            <span className="absolute left-0 bottom-[-0.55556rem] bg-yellow-500 rounded-[0.11111rem] content-[''] h-[0.22222rem] transition-all duration-250 ease w-[1.83333rem]"></span>
             </h3>
           </div>
           <div className="p-6  shadow-lg rounded-lg relative">
             <h3 className="text-xl font-semibold mb-4 relative">Web Development
-            <span className="absolute left-0 bottom-[-0.55556rem] bg-[#e7e729] rounded-[0.11111rem] content-[''] h-[0.22222rem] transition-all duration-250 ease w-[1.83333rem]"></span>
+            <span className="absolute left-0 bottom-[-0.55556rem] bg-yellow-500 rounded-[0.11111rem] content-[''] h-[0.22222rem] transition-all duration-250 ease w-[1.83333rem]"></span>
             </h3>
           </div>
           <div className="p-6  shadow-lg rounded-lg relative">
             <h3 className="text-xl font-semibold mb-4 relative">eCommerce solutions
-            <span className="absolute left-0 bottom-[-0.55556rem] bg-[#e7e729] rounded-[0.11111rem] content-[''] h-[0.22222rem] transition-all duration-250 ease w-[1.83333rem]"></span>
+            <span className="absolute left-0 bottom-[-0.55556rem] bg-yellow-500 rounded-[0.11111rem] content-[''] h-[0.22222rem] transition-all duration-250 ease w-[1.83333rem]"></span>
             </h3>
           </div>
           <div className="p-6  shadow-lg rounded-lg relative">
             <h3 className="text-xl font-semibold mb-4 relative">SEO optimization
-            <span className="absolute left-0 bottom-[-0.55556rem] bg-[#e7e729] rounded-[0.11111rem] content-[''] h-[0.22222rem] transition-all duration-250 ease w-[1.83333rem]"></span>
+            <span className="absolute left-0 bottom-[-0.55556rem] bg-yellow-500 rounded-[0.11111rem] content-[''] h-[0.22222rem] transition-all duration-250 ease w-[1.83333rem]"></span>
             </h3>
           </div>
           <div className="p-6  shadow-lg rounded-lg relative">
             <h3 className="text-xl font-semibold mb-4 relative">Automatisation
-            <span className="absolute left-0 bottom-[-0.55556rem] bg-[#e7e729] rounded-[0.11111rem] content-[''] h-[0.22222rem] transition-all duration-250 ease w-[1.83333rem]"></span>
+            <span className="absolute left-0 bottom-[-0.55556rem] bg-yellow-500 rounded-[0.11111rem] content-[''] h-[0.22222rem] transition-all duration-250 ease w-[1.83333rem]"></span>
             </h3>
           </div>
         </div>
