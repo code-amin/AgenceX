22e5b36551284825345ddaa04f6e964ee510255a
AlphaHelloWorld <73975219+AlphaHelloWorld@users.noreply.github.com>
2024-01-15 16:32:15 -0500

    arguments old changes

--- agence/src/Arguments.js
+++ agence/src/Arguments.js
@@ -1,48 +1,52 @@
 const Arguments = () => {
   return (
     <div id="arguments">
-      <h2 className="text-3xl font-bold text-center mb-4">
-        Why work with the 🦄 Lab{" "}
+      <h2 className="font-bold text-center mb-4 text-3xl lg:text-4xl">
+        Pourquoi choisir <span className="font-medium">Licorne</span>
+        <span className="font-thin">Labs</span> 🦄
       </h2>
       <div className="flex flex-col md:flex-row gap-4 justify-center">
         <div className="bg-gray-800 p-6 rounded-lg">
           <h3 className="text-white-500 text-2xl mb-4">
-            Our Expertise and Innovation:
+            L'Innovation est notre raison d'être.
           </h3>
           <p className="text-white-500 text-sm mb-4">
-            At Licorne Labs, we bring a wealth of expertise to your project. Our
-            seasoned development team is well-versed in the latest technologies,
-            frameworks, and design trends. When you choose us, you're partnering
-            with a team dedicated to innovation, ensuring that your web
-            solutions are not just current but on the cutting edge.o
+            Chez Licorne Labs, nous apportons une richesse d'expertise à votre
+            projet. Notre équipe de développement chevronnée maîtrise les
+            dernières technologies, frameworks et tendances en matière de
+            design. En choisissant notre équipe, vous vous associez à une équipe
+            dédiée à l'innovation, garantissant que vos solutions web ne sont
+            pas seulement actuelles, mais à la pointe de la technologie.
           </p>
         </div>
         <div className="bg-gray-800 p-6 rounded-lg">
           <h3 className="text-2xl text-white-500 mb-4">
-            Tailored Solutions for Your Success
+            Des Solutions Adaptées pour Votre Réussite
           </h3>
           <p className="text-white-500 text-sm mb-4">
-            We take the time to understand your unique needs and business goals.
-            At Licorne Labs we pride ourselves on crafting tailored web
-            solutions that align perfectly with your objectives. Our
-            collaborative approach ensures that every project is a bespoke
-            reflection of your vision, driving tangible results, whether it's
-            improved user engagement, increased conversions, or enhanced brand
-            visibility.
+            Nous prenons le temps de comprendre vos besoins uniques et vos
+            objectifs commerciaux. Chez Licorne Labs, nous nous enorgueillissons
+            de créer des solutions web sur mesure qui correspondent parfaitement
+            à vos objectifs. Notre approche collaborative garantit que chaque
+            projet est une réflexion sur mesure de votre vision, générant des
+            résultats tangibles, que ce soit une amélioration de l'engagement
+            des utilisateurs, une augmentation des conversions ou une meilleure
+            visibilité de la marque.
           </p>
         </div>
         <div className="bg-gray-800 p-6 rounded-lg">
           <h3 className="text-2xl text-white-500 mb-4">
-            Exceptional Support and Collaboration{" "}
+            Un Support Exceptionnel et une Collaboration de Qualité
           </h3>
           <p className="text-white-500 text-sm mb-4">
-            Experience a client-centric journey with Licorne Labs. Our
-            commitment to exceptional customer support means you're not just a
-            client; you're a valued partner. We prioritize open communication,
-            transparency, and responsiveness. Throughout the development
-            process, we actively seek your input, value your feedback, and
-            collaborate closely to ensure the final product exceeds your
-            expectations.{" "}
+            Vivez une expérience centrée sur le client avec Licorne Labs. Notre
+            engagement envers un support client exceptionnel signifie que vous
+            n'êtes pas simplement un client ; vous êtes un partenaire précieux.
+            Nous accordons la priorité à la communication ouverte, à la
+            transparence et à la réactivité. Tout au long du processus de
+            développement, nous recherchons activement votre contribution,
+            apprécions vos commentaires et collaborons étroitement pour nous
+            assurer que le produit final dépasse vos attentes.
           </p>
         </div>
       </div>
