f40bf8f9a37c4e31b3ec18018ccefd8f4aff2b32
AlphaHelloWorld <73975219+AlphaHelloWorld@users.noreply.github.com>
2024-01-26 10:49:55 -0500

    fixed form positioning on mobile

--- agence/src/Contact.js
+++ agence/src/Contact.js
@@ -27,73 +27,74 @@ const Contact = ({ plan }) => {
   };
 
   return (
-    <form
-      id="contact"
-      method="POST"
-      className="max-w-3xl mt-24 mx-auto bg-white rounded-lg p-4 text-black"
-      name="clientForm"
-      onSubmit={handleSubmit}
-    >
-      <input type="hidden" name="form-name" value="clientForm" />
+    <div className="pt-24" id="contact">
+      <form
+        method="POST"
+        className="max-w-3xl mt-24 mx-auto bg-white rounded-lg p-4 text-black"
+        name="clientForm"
+        onSubmit={handleSubmit}
+      >
+        <input type="hidden" name="form-name" value="clientForm" />
 
-      <h2 className="text-3xl font-bold mb-4 text-center">Contactez-nous</h2>
-      <div className="my-4">
-        <div className="flex flex-nowrap gap-4">
+        <h2 className="text-3xl font-bold mb-4 text-center">Contactez-nous</h2>
+        <div className="my-4">
+          <div className="flex flex-nowrap gap-4">
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              required
+              placeholder="Prénom"
+              className="p-2 w-full md:w-1/2 border rounded-md"
+              title="Entrez un prénom."
+            />
+            <input
+              type="text"
+              id="lastName"
+              name="lastName"
+              required
+              placeholder="Nom"
+              className="p-2 w-full md:w-1/2 border rounded-md"
+              title="Entrez un nom de famille."
+            />
+          </div>
           <input
+            minLength="4"
             type="text"
-            id="firstName"
-            name="firstName"
+            id="contactInfo"
+            name="contactInfo"
             required
-            placeholder="Prénom"
-            className="p-2 w-full md:w-1/2 border rounded-md"
-            title="Entrez un prénom."
+            placeholder="Courriel ou numéro de tel."
+            className="mt-1 p-2 w-full border rounded-md"
+            pattern="^([0-9]{10})|([0-9]{3}\+\-[0-9]{3}\+\-[0-9]{3})|([A-Za-z0-9._%\+\-]+@[a-z0-9.\-]+\.[a-z]{2,5})$"
+            title="Entrez un courriel ou numéro de tél."
           />
           <input
             type="text"
-            id="lastName"
-            name="lastName"
+            id="plan"
+            name="plan"
             required
-            placeholder="Nom"
-            className="p-2 w-full md:w-1/2 border rounded-md"
-            title="Entrez un nom de famille."
+            placeholder={`${plan}`}
+            className="mt-1 p-2 w-full border rounded-md"
+          />
+          <textarea
+            type="text"
+            id="comment"
+            name="comment"
+            placeholder="Commentaire, question..."
+            className="mt-1 p-2 w-full border rounded-md resize-both overflow-auto"
           />
         </div>
-        <input
-          minLength="4"
-          type="text"
-          id="contactInfo"
-          name="contactInfo"
-          required
-          placeholder="Courriel ou numéro de tel."
-          className="mt-1 p-2 w-full border rounded-md"
-          pattern="^([0-9]{10})|([0-9]{3}\+\-[0-9]{3}\+\-[0-9]{3})|([A-Za-z0-9._%\+\-]+@[a-z0-9.\-]+\.[a-z]{2,5})$"
-          title="Entrez un courriel ou numéro de tél."
-        />
-        <input
-          type="text"
-          id="plan"
-          name="plan"
-          required
-          placeholder={`${plan}`}
-          className="mt-1 p-2 w-full border rounded-md"
-        />
-        <textarea
-          type="text"
-          id="comment"
-          name="comment"
-          placeholder="Commentaire, question..."
-          className="mt-1 p-2 w-full border rounded-md resize-both overflow-auto"
-        />
-      </div>
 
-      <button
-        type="submit"
-        className="bg-yellow-500 text-white p-2 block mx-auto w-2/5 py-2 px-4 mt-6 rounded-md hover:bg-yellow-400"
-        onClick={checkPlaceholder}
-      >
-        Soumettre
-      </button>
-    </form>
+        <button
+          type="submit"
+          className="bg-yellow-500 text-white p-2 block mx-auto w-2/5 py-2 px-4 mt-6 rounded-md hover:bg-yellow-400"
+          onClick={checkPlaceholder}
+        >
+          Soumettre
+        </button>
+      </form>
+    </div>
   );
 };
 
