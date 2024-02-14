b28cdf4b05d1511c68f19160d95c4c8fa166623a
AlphaHelloWorld <73975219+AlphaHelloWorld@users.noreply.github.com>
2024-02-14 11:15:23 -0500

    Agence X

--- agence/src/Socials.js
+++ agence/src/Socials.js
@@ -1,24 +1,49 @@
-import { FaTwitter, FaFacebook, FaInstagram, FaLinkedin, FaYoutube } from 'react-icons/fa';
-
+import {
+  FaTwitter,
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaYoutube,
+} from "react-icons/fa";
 
 const Socials = () => {
   return (
     <div className="bg-gray-900 text-white py-8">
       <h2 className="text-2xl font-bold text-center mb-8">Suivez-nous</h2>
       <div className="flex justify-center">
-        <a href="https://www.facebook.com/LicorneLabs" target="_blank" className="text-yellow-500 hover:text-yellow-400 mx-4 text-4xl">
+        <a
+          href="https://www.facebook.com/Agence X"
+          target="_blank"
+          className="text-yellow-500 hover:text-yellow-400 mx-4 text-4xl"
+        >
           <FaFacebook />
         </a>
-        <a href="https://www.youtube.com/@licornelabs" target="_blank" className="text-yellow-500 hover:text-yellow-400 mx-4 text-4xl">
+        <a
+          href="https://www.youtube.com/@Agence X"
+          target="_blank"
+          className="text-yellow-500 hover:text-yellow-400 mx-4 text-4xl"
+        >
           <FaYoutube />
         </a>
-        <a href="https://twitter.com/licornelabs" target="_blank" className="text-yellow-500 hover:text-yellow-400 mx-4 text-4xl">
+        <a
+          href="https://twitter.com/Agence X"
+          target="_blank"
+          className="text-yellow-500 hover:text-yellow-400 mx-4 text-4xl"
+        >
           <FaTwitter />
         </a>
-        <a href="https://www.instagram.com/licornelabs/" target="_blank" className="text-yellow-500 hover:text-yellow-400 mx-4 text-4xl">
+        <a
+          href="https://www.instagram.com/Agence X/"
+          target="_blank"
+          className="text-yellow-500 hover:text-yellow-400 mx-4 text-4xl"
+        >
           <FaInstagram />
         </a>
-        <a href="https://www.linkedin.com/company/licornelabs" target="_blank" className="text-yellow-500 hover:text-yellow-400 mx-4 text-4xl">
+        <a
+          href="https://www.linkedin.com/company/Agence X"
+          target="_blank"
+          className="text-yellow-500 hover:text-yellow-400 mx-4 text-4xl"
+        >
           <FaLinkedin />
         </a>
       </div>
@@ -27,5 +52,3 @@ const Socials = () => {
 };
 
 export default Socials;
-
-  
